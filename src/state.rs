594990d3d164549956412e8796f6ba231319357f
Domi <domitillevale@gmail.com>
2026-03-03 19:35:45 +0100

    ServerMsg State in update done but untested.

--- src/state.rs
+++ src/state.rs
@@ -52,39 +52,91 @@ pub struct AgentInfo {
 //   - agents: Vec<AgentInfo>
 //   - team_scores: HashMap<String, u32>
 //
-// pub struct GameState {
-//     ...
-// }
+pub struct GameState {
+    agent_id: Uuid,
+    tick: u64,
+    position: (u16, u16),
+    map_size: (u16, u16),
+    goal: u32,
+    obstacles: Vec<(u16, u16)>,
+    resources: Vec<ResourceInfo>,
+    agents: Vec<AgentInfo>,
+    team_scores: HashMap<String, u32>
+}
 
 // TODO: Implémenter GameState.
 //
-// impl GameState {
-//     /// Crée un état initial avec l'agent_id reçu du serveur.
-//     pub fn new(agent_id: Uuid) -> Self {
-//         ...
-//     }
-//
-//     /// Met à jour l'état à partir d'un message serveur.
-//     ///
-//     /// Doit gérer au minimum :
-//     ///   - ServerMsg::State { .. } → mettre à jour tick, position, resources, agents, etc.
-//     ///     Indice : votre position est dans la liste `agents`, trouvez-la par agent_id.
-//     ///   - ServerMsg::PowResult { resource_id, .. } → retirer la ressource de la liste.
-//     ///
-//     /// Les autres messages peuvent être ignorés ici.
-//     pub fn update(&mut self, msg: &ServerMsg) {
-//         ...
-//     }
-// }
+impl GameState {
+    /// Crée un état initial avec l'agent_id reçu du serveur.
+    pub fn new(agent_id: Uuid) -> Self {
+        GameState{
+            agent_id: agent_id,
+            tick: 0,
+            position: (0, 0),
+            map_size: (0, 0),
+            goal: 0,
+            obstacles: vec![],
+            resources: vec![],
+            agents: vec![],
+            team_scores: HashMap::new()
+        }
+    }
+
+    /// Met à jour l'état à partir d'un message serveur.
+    ///
+    /// Doit gérer au minimum :
+    ///   - ServerMsg::State { .. } → mettre à jour tick, position, resources, agents, etc.
+    ///     Indice : votre position est dans la liste `agents`, trouvez-la par agent_id.
+    ///   - ServerMsg::PowResult { resource_id, .. } → retirer la ressource de la liste.
+    ///
+    /// Les autres messages peuvent être ignorés ici.
+    pub fn update(&mut self, msg: &ServerMsg) {
+        match msg.clone() {
+            ServerMsg::State { tick, width, height, goal, obstacles, resources, agents } => {
+                self.tick = tick;
+                self.map_size = (width, height);
+                self.goal = goal;
+                self.obstacles = obstacles;
+                for resource in resources {
+                    self.resources.push(ResourceInfo{
+                        resource_id: resource.0,
+                        x: resource.1, 
+                        y:resource.2, 
+                        expires_at: resource.3,
+                        value: resource.4}
+                    );
+                }
+                for agent in agents {
+                    if agent.0 == self.agent_id {
+                        self.position = (agent.4, agent.5);
+                    } else {
+                        self.agents.push(AgentInfo { 
+                            id: agent.0, 
+                            name: agent.1, 
+                            team: agent.2, 
+                            score: agent.3, 
+                            x: agent.4, 
+                            y: agent.5 }
+                        );
+                    }
+                }
+            } 
+            ServerMsg::PowResult { resource_id, winner } => {
+            }  
+            _ => {}
+        }      
+    }
+}
+
 
 // TODO: Définir le type alias SharedState.
 //
 // C'est un Arc<Mutex<GameState>> pour pouvoir le partager entre threads.
 //
-// pub type SharedState = Arc<Mutex<GameState>>;
+pub type SharedState = Arc<Mutex<GameState>>;
 //
 // Ajoutez une fonction de construction pratique :
 //
-// pub fn new_shared_state(agent_id: Uuid) -> SharedState {
-//     Arc::new(Mutex::new(GameState::new(agent_id)))
-// }
+pub fn new_shared_state(agent_id: Uuid) -> SharedState {
+    Arc::new(Mutex::new(GameState::new(agent_id)))
+}
